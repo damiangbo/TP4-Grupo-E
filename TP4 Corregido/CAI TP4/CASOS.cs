274ffc0e49f9b0f7569ed2808a0117a8b5ff2192
damiangbo <64539474+damiangbo@users.noreply.github.com>
2021-06-12 01:45:18 -0300

    Add files via upload

---
+++ TP4 Corregido/CAI TP4/CASOS.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAI_TP4
+{
+    class CASOS
+    {
+        public static string rutaCursosSistemas = @"C:\Users\marin\Documents\CURSOSSISTEMAS.csv";
+        public static string rutaCursosContador = @"C:\Users\marin\Documents\CURSOSCONTADOR.csv";
+        public static string rutaCursosAdmin = @"C:\Users\marin\Documents\CURSOSADMIN.csv";
+        public static string rutaCursosEconomia = @"C:\Users\marin\Documents\CURSOSECONOMIA.csv";
+        public static string rutaCursosActuario = @"C:\Users\marin\Documents\CURSOSACTUARIO.csv";
+
+        public static string rutaMateriasSistemas = @"C:\Users\marin\Documents\MATERIASSISTEMAS.csv";
+        public static string rutaMateriasContador = @"C:\Users\marin\Documents\MATERIASCONTADOR.csv";
+        public static string rutaMateriasAdmin = @"C:\Users\marin\Documents\MATERIASADMIN.csv";
+        public static string rutaMateriasEconomia = @"C:\Users\marin\Documents\MATERIASECONOMIA.csv";
+        public static string rutaMateriasActuario = @"C:\Users\marin\Documents\MATERIASACTUARIO.csv";
+
+        public static void Case1()
+        {
+            string nombreAlumno = "";
+            int numero1 = 1;
+            int numero2 = 5;
+            bool esRegistroValido;
+            bool opcionCarreraValida;
+
+            do
+            {
+                Console.Write("Ingrese su número de registro: ");
+                string registroIng = Console.ReadLine();
+                esRegistroValido = VALIDACION.ValidarRegistro(registroIng);
+                if (!esRegistroValido)
+                {
+                    continue;
+                }
+                Console.WriteLine("Número de registro válido.");
+                INSCRIPCIONES.MostrarAlumno(registroIng, ref nombreAlumno);
+                Console.WriteLine($"Hola {nombreAlumno}!\n");
+                do
+                {
+                    Console.WriteLine("Seleccione un número correspondiente a su carrera para inscribirse: ");
+                    Console.WriteLine("'1' Contador Público");
+                    Console.WriteLine("'2' Lic. Administación");
+                    Console.WriteLine("'3' Lic. Economía");
+                    Console.WriteLine("'4' Lic. Sistemas");
+                    Console.WriteLine("'5' Actuario");
+                    string opcionCarrera = Console.ReadLine();
+                    opcionCarreraValida = VALIDACION.ValidarMenu(opcionCarrera, numero1, numero2);
+                    if (opcionCarreraValida == false)
+                    {
+                        continue;
+                    }
+                    if (INSCRIPCIONES.CarreraEnArchivoIgual(registroIng, opcionCarrera) == false)
+                    {
+                        opcionCarreraValida = false;
+                        continue;
+                    }
+                    else
+                    {
+                        if (opcionCarrera == "1")
+                        {
+                            VALIDACION.Carrera = 1;
+                            INSCRIPCIONES.ParaTodasLasCarreras(registroIng, rutaMateriasContador, rutaCursosContador);
+                        }
+                        if (opcionCarrera == "2")
+                        {
+                            VALIDACION.Carrera = 2;
+                            INSCRIPCIONES.ParaTodasLasCarreras(registroIng, rutaMateriasAdmin, rutaCursosAdmin);
+                        }
+                        if (opcionCarrera == "3")
+                        {
+                            VALIDACION.Carrera = 3;
+                            INSCRIPCIONES.ParaTodasLasCarreras(registroIng, rutaMateriasEconomia, rutaCursosEconomia);
+                        }
+                        if (opcionCarrera == "4")
+                        {
+                            VALIDACION.Carrera = 4;
+                            INSCRIPCIONES.ParaTodasLasCarreras(registroIng, rutaMateriasSistemas, rutaCursosSistemas);
+                        }
+                        if (opcionCarrera == "5")
+                        {
+                            VALIDACION.Carrera = 5;
+                            INSCRIPCIONES.ParaTodasLasCarreras(registroIng, rutaMateriasActuario, rutaCursosActuario);
+                        }
+                    }
+                } while (!opcionCarreraValida);
+
+            } while (!esRegistroValido);
+        }
+
+        public static void Case2()
+        {
+            bool estaInscripto;
+            bool esRegistroValido;
+            do
+            {
+                Console.WriteLine("Ingrese un número de registro para obtener el estado actual de inscripción: ");
+                string registroIng = Console.ReadLine();
+                esRegistroValido = VALIDACION.ValidarRegistro(registroIng);
+                if (!esRegistroValido)
+                {
+                    continue;
+                }
+
+                string materiasYcursos = "";
+                estaInscripto = INSCRIPCIONES.InscripcionesPorReg(registroIng, ref materiasYcursos);
+                if (estaInscripto)
+                {
+                    Console.WriteLine($"El registro '{registroIng}' solicitó la inscripción en: \n{materiasYcursos}" +
+                        $"\nRecuerde que el código de carrera corresponde a lo siguiente: \n '1' Contador Público\n '2' Lic. Administación\n '3' Lic. Economía\n '4' Lic. Sistemas\n '5' Actuario\n");
+                    break;
+                }
+                if (!estaInscripto)
+                {
+                    Console.WriteLine($"El registro '{registroIng}' no solicitó la inscripción en ninguna materia.\n");
+                    break;
+                }
+
+            } while (!esRegistroValido);
+        }
+
+        public static void Case3()
+        {
+            string lista = "";
+            int numero1 = 1;
+            int numero2 = 5;
+            bool opcionCarreraValida;
+
+            do
+            {
+                Console.WriteLine("Seleccione un número correspondiente a la carrera para consultar las materias disponibles:  ");
+                Console.WriteLine("'1' Contador Público");
+                Console.WriteLine("'2' Lic. Administación");
+                Console.WriteLine("'3' Lic. Economía");
+                Console.WriteLine("'4' Lic. Sistemas");
+                Console.WriteLine("'5' Actuario");
+                string opcionCarrera = Console.ReadLine();
+                opcionCarreraValida = VALIDACION.ValidarMenu(opcionCarrera, numero1, numero2);
+                if (!opcionCarreraValida)
+                {
+                    continue;
+                }
+                else
+                {
+                    if (opcionCarrera == "1")
+                    {
+                        INSCRIPCIONES.MateriasYCodigos(rutaMateriasContador, ref lista);
+                        Console.WriteLine("\nListado de materias disponibles de la carrera de Contador Público: \n");
+                        Console.WriteLine(lista);
+                        Console.WriteLine("Para el plan de materias ingrese a 'www.economicas.uba.ar/carreras/sistemas/'.\n");
+                    }
+                    if (opcionCarrera == "2")
+                    {
+                        INSCRIPCIONES.MateriasYCodigos(rutaMateriasAdmin, ref lista);
+                        Console.WriteLine("\nListado de materias disponibles de la carrera de Administración: \n");
+                        Console.WriteLine(lista);
+                        Console.WriteLine("Para el plan de materias ingrese a 'www.economicas.uba.ar/carreras/sistemas/'.\n");
+                    }
+                    if (opcionCarrera == "3")
+                    {
+                        INSCRIPCIONES.MateriasYCodigos(rutaMateriasEconomia, ref lista);
+                        Console.WriteLine("\nListado de materias disponibles de la carrera de Economía: \n");
+                        Console.WriteLine(lista);
+                        Console.WriteLine("Para el plan de materias ingrese a 'www.economicas.uba.ar/carreras/sistemas/'.\n");
+                    }
+                    if (opcionCarrera == "4")
+                    {
+                        INSCRIPCIONES.MateriasYCodigos(rutaMateriasSistemas, ref lista);
+                        Console.WriteLine("\nListado de materias disponibles de la carrera de Sistemas: \n");
+                        Console.WriteLine(lista);
+                        Console.WriteLine("Para el plan de materias ingrese a 'www.economicas.uba.ar/carreras/sistemas/'.\n");
+                    }
+                    if (opcionCarrera == "5")
+                    {
+                        INSCRIPCIONES.MateriasYCodigos(rutaMateriasActuario, ref lista);
+                        Console.WriteLine("\nListado de materias disponibles de la carrera de Actuario: \n");
+                        Console.WriteLine(lista);
+                        Console.WriteLine("Para el plan de materias ingrese a 'www.economicas.uba.ar/carreras/sistemas/'.\n");
+                    }
+                }
+            } while (!opcionCarreraValida);
+
+        }
+
+        public static void Case4()
+        {
+            string lista = "";
+            int numero1 = 1;
+            int numero2 = 5;
+            bool opcionCarreraValida;
+
+            do
+            {
+                Console.WriteLine("Seleccione un número correspondiente a la carrera para consultar los profesores por curso disponibles:  ");
+                Console.WriteLine("'1' Contador Público");
+                Console.WriteLine("'2' Lic. Administación");
+                Console.WriteLine("'3' Lic. Economía");
+                Console.WriteLine("'4' Lic. Sistemas");
+                Console.WriteLine("'5' Actuario");
+                string opcionCarrera = Console.ReadLine();
+                opcionCarreraValida = VALIDACION.ValidarMenu(opcionCarrera, numero1, numero2);
+                if (!opcionCarreraValida)
+                {
+                    continue;
+                }
+                else
+                {
+                    if (opcionCarrera == "1")
+                    {
+                        INSCRIPCIONES.ProfesoresPorCurso(rutaCursosContador, ref lista);
+                        Console.WriteLine("\nListado de profesores por materia y curso de la carrera de Contador Público: \n");
+                        Console.WriteLine(lista);
+                    }
+                    if (opcionCarrera == "2")
+                    {
+                        INSCRIPCIONES.ProfesoresPorCurso(rutaCursosAdmin, ref lista);
+                        Console.WriteLine("\nListado de profesores por materia y curso de la carrera de Administración: \n");
+                        Console.WriteLine(lista);
+                    }
+                    if (opcionCarrera == "3")
+                    {
+                        INSCRIPCIONES.ProfesoresPorCurso(rutaCursosEconomia, ref lista);
+                        Console.WriteLine("\nListado de profesores por materia y curso de la carrera de Economía: \n");
+                        Console.WriteLine(lista);
+                    }
+                    if (opcionCarrera == "4")
+                    {
+                        INSCRIPCIONES.ProfesoresPorCurso(rutaCursosSistemas, ref lista);
+                        Console.WriteLine("\nListado de profesores por materia y curso de la carrera de Sistemas: \n");
+                        Console.WriteLine(lista);
+                    }
+                    if (opcionCarrera == "5")
+                    {
+                        INSCRIPCIONES.ProfesoresPorCurso(rutaCursosActuario, ref lista);
+                        Console.WriteLine("\nListado de profesores por materia y curso de la carrera de Actuario: \n");
+                        Console.WriteLine(lista);
+                    }
+                }
+            } while (!opcionCarreraValida);
+        }
+
+        public static void Case0()
+        {
+            Console.WriteLine("\n \n       Muchas gracias por utilizar el sistema de inscripción de la FCE - UBA." +
+                               "\n                     Te esperamos para la próxima inscripción!" +
+                                "\n\n                      >Presione una tecla para salir<");
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ TP4 Corregido/CAI TP4/VALIDACION.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAI_TP4
+{
+    class VALIDACION
+    {
+        public static int Registro = 0;
+        public static int Carrera = 0;
+        public static string Opcion = "";
+        public static int Materia = 0;
+        public static int Curso = 0;
+
+        public static bool ValidarMenu(string opcionMenu, int numero1, int numero2)
+        {
+            // Validación tipo de dato de opción
+            if (!int.TryParse(opcionMenu, out int opcionValidada))
+            {
+                Console.WriteLine($"La opción ingresada '{opcionMenu}', es inválida. No ha ingresado un número.\n");
+                return false;
+            }
+            // Validación de rango de opción
+            else if (opcionValidada < numero1 || opcionValidada > numero2)
+            {
+                Console.WriteLine($"El número de opción ingresado '{opcionValidada}', es inválido. Debe ingresar un número que se encuentre en el menú.\n");
+                return false;
+            }
+            return true;
+        }
+
+        public static bool ValidarRegistro(string codigoRegistro)
+        {
+            // Validación de tipo de dato
+            bool IngresoCorrecto = Int32.TryParse(codigoRegistro, out int codigo);
+
+            if (!IngresoCorrecto)
+            {
+                Console.WriteLine($"El registro ingresado '{codigoRegistro}', es incorrecto. Debe ingresar un número.\n");
+                return false;
+            }
+            // Validación de rango
+            int i = 10000;
+            int j = 10025;
+            bool estaEnElRango = codigo >= i && codigo <= j;
+
+            if (!estaEnElRango)
+            {
+                Console.WriteLine($"El número de registro ingresado '{codigo}', no está dentro del rango válido. \nRecuerde que su registro debe estar entre {i} y {j}.\n");
+                return false;
+            }
+            Registro = codigo;
+            return true;
+        }
+
+        public static bool ValidarOpcion(string ultimas4Materias)
+        {
+            if (ultimas4Materias == "S" || ultimas4Materias == "N")
+            {
+                Opcion = ultimas4Materias;
+                return true;
+            }
+            else
+            {
+                Console.WriteLine($"La opción ingresada '{ultimas4Materias}', es inválida (no es un caracter válido). Debe ingresar 'S' o 'N'.\n");
+                return false;
+            }
+        }
+
+        public static bool ValidarMateria(string codigoMateria)
+        {
+            // Validación de tipo de dato
+            bool IngresoCorrecto = Int32.TryParse(codigoMateria, out int codigo);
+            int p = 301;
+            int r = 312;
+
+            if (!IngresoCorrecto)
+            {
+                Console.WriteLine($"El código de materia ingresado '{codigoMateria}', es incorrecto. Debe ingresar un número.\n");
+                return false;
+
+            }
+
+            // Validación de rango
+            bool estaEnElRango = codigo >= p && codigo <= r;
+
+            if (!estaEnElRango)
+            {
+                Console.WriteLine($"El código de materia ingresado '{codigo}', no está en un rango válido. \nRecuerde que el código de materia debe estar entre {p} y {r}.\n");
+                return false;
+            }
+
+            Materia = codigo;
+            return true;
+        }
+
+        public static bool ValidarCurso(string codigoCurso)
+        {
+            // Validación de tipo de dato
+            bool IngresoCorrecto = Int32.TryParse(codigoCurso, out int codigo);
+            int x = 1;
+            int y = 2;
+
+            if (!IngresoCorrecto)
+            {
+                Console.WriteLine($"El código de curso ingresado '{codigoCurso}', es incorrecto. Debe ingresar un número.\n");
+                return false;
+
+            }
+
+            // Validación de rango
+            bool estaEnElRango = codigo >= x && codigo <= y;
+
+            if (!estaEnElRango)
+            {
+                Console.WriteLine($"El código de curso ingresado '{codigo}', no está dentro del rango válido. Recuerde que el código de curso debe ser '{x}' o '{y}'.\n");
+                return false;
+            }
+
+            Curso = codigo;
+            return true;
+        }
+    }
+}
